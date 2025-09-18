4335b61dc7a5409568485a0761c5cdac9b9dc7a4
walid.gbil <walid.gbil@student.vinci.be>
2025-09-18 14:35:36 +0200

    tutorials pour drinks

---
+++ tutorials/back/api/basic/app.ts
@@ -0,0 +1,24 @@
+import express, { ErrorRequestHandler } from "express";
+
+import usersRouter from "./routes/users";
+import pizzaRouter from "./routes/pizzas";
+import drinksRouter from "./routes/drinks";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/users", usersRouter);
+app.use("/pizzas", pizzaRouter);
+app.use("/drinks",drinksRouter);
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  console.error(err.stack);
+  return res.status(500).send("Something broke!");
+};
+
+app.use(errorHandler);
+
+export default app;
+
